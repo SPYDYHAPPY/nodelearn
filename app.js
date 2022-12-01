d42f5a1b5acdee24cb2f1aba2bb35bbdc858d5b1
SPYDYHAPPY <spiderhappy5@gmail.com>
2022-12-01 21:39:02 +0530

    Update

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+//use path module
+const path = require('path');
+//use express module
 const express = require('express');
+//use hbs view engine
+const hbs = require('hbs');
 const app = express();
+
+
+//set dynamic views file
+app.set('views',path.join(__dirname,'views'));
+//set view engine
+app.set('view engine','hbs');
+//set public folder as static folder static file
+app.use(express.static('public'));
 
 //route for home page
 app.get('/',(req, res) =>{
-    res.send('welcome to home page');
+    res.render('index');
 });
 
 //route for about page
