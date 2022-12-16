a97979a3bfe508e6920d2be3dd139c66aeda7acf
Debasish Brahma <dbrahma4@outlook.com>
2022-12-16 23:45:52 +0530

    login updated

--- public/js/login.js
+++ public/js/login.js
@@ -34,39 +34,44 @@ document.getElementById('login_btn').addEventListener('click',
 
 
         if (p_mail == "" || p_pass == "") {
-            document.getElementById("login_notification").innerHTML = "Invalid email id";
+            document.getElementById("login_notification").innerHTML = "May be your email address or password is missing.";
             document.getElementById("login_notification").style.color = "#F95559";
            
             setTimeout(() => {
                 window.location.reload();
-            }, 1000);
+            }, 800);
 
         } else if (db_list.p_email != p_mail) {
             //console.log('password does not match.')
-            document.getElementById("login_notification").innerHTML = "email does not match";
+            document.getElementById("login_notification").innerHTML = "email address does not match";
             document.getElementById("login_notification").style.color = "#F95559";
 
 
             setTimeout(() => {
                 window.location.reload();
-            }, 1000);
+            }, 800);
 
         } else if (db_list.p_pass != p_pass) {
-            document.getElementById("login_notification").innerHTML = "Invalid password";
+            document.getElementById("login_notification").innerHTML = "Incorrect password";
             document.getElementById("login_notification").style.color = "#F95559";
 
 
             setTimeout(() => {
                 window.location.reload();
-            }, 5000);
+            }, 800);
 
         } else {
-            document.getElementById("login_notification").innerHTML = "login success. you will be redirect to dashboard soon";
+            document.getElementById("login_notification").innerHTML = `login success. you will be 
+            redirect to dashboard soon....
+            <div class="spinner-grow spinner-grow-sm text-success" role="status">
+            <span class="visually-hidden">Loading...</span>
+            </div>`;
             document.getElementById("login_notification").style.color = "#37733E";
 
 
             setTimeout(() => {
-                window.location.replace("http://localhost:3000/user-dashboard");
-            }, 3000);
+                //window.location.replace("http://localhost:3000/user-dashboard");
+                window.location.href = "user-dashboard";
+            }, 2000);
         }
     });
