55fd2f42af76c26d52147f3c1cbb4658b387cf0c
SPYDYHAPPY <spiderhappy5@gmail.com>
2022-12-19 19:54:45 +0530

    all js updated

---
+++ public/js/seller.js
@@ -0,0 +1,177 @@
+const urb = document.getElementById("urbanic");
+const semurb = document.getElementById("semi_urbanic");
+
+urb.innerHTML = `<table class="table">
+<thead>
+    <tr>
+        <th scope="col">SELLER ID</th>
+        <th scope="col">SELLER NAME</th>
+        <th scope="col">GENDER</th>
+        <th scope="col">CITY</th>
+        <th scope="col">RAITING</th>
+        <th scope="col">REGISTER DATE</th>
+        <th scope="col">ACTION</th>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+        <th scope="row">000121</th>
+        <td>SALMAN KHAN</td>
+        <td>MALE</td>
+        <td>MUMBAI</td>
+        <td>
+        <!-- Product reviews-->
+        <div class="d-flex justify-content-start small text-warning mb-2">
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+        </div>
+        </td>
+        <td>12/05/2020</td>
+        <td><button type="button" class="btn btn-secondary btn-sm">VIEW</button>
+        </td>
+    </tr>
+    <tr>
+        <th scope="row">000121</th>
+        <td>SALMAN KHAN</td>
+        <td>MALE</td>
+        <td>MUMBAI</td>
+        <td>
+        <!-- Product reviews-->
+        <div class="d-flex justify-content-start small text-warning mb-2">
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+        </div>
+        </td>
+        <td>12/05/2020</td>
+        <td><button type="button" class="btn btn-secondary btn-sm">VIEW</button>
+        </td>
+    </tr><tr>
+        <th scope="row">000121</th>
+        <td>SALMAN KHAN</td>
+        <td>MALE</td>
+        <td>MUMBAI</td>
+        <td>
+        <!-- Product reviews-->
+        <div class="d-flex justify-content-start small text-warning mb-2">
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+        </div>
+        </td>
+        <td>12/05/2020</td>
+        <td><button type="button" class="btn btn-secondary btn-sm">VIEW</button>
+        </td>
+    </tr>
+    <tr>
+        <th scope="row">000121</th>
+        <td>SALMAN KHAN</td>
+        <td>MALE</td>
+        <td>MUMBAI</td>
+        <td>
+        <!-- Product reviews-->
+        <div class="d-flex justify-content-start small text-warning mb-2">
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+        </div>
+        </td>
+        <td>12/05/2020</td>
+        <td><button type="button" class="btn btn-secondary btn-sm">VIEW</button>
+        </td>
+    </tr>
+
+</tbody>
+</table>`;
+semurb.innerHTML =`<table class="table">
+<thead>
+    <tr>
+        <th scope="col">SELLER ID</th>
+        <th scope="col">SELLER NAME</th>
+        <th scope="col">GENDER</th>
+        <th scope="col">CITY</th>
+        <th scope="col">RAITING</th>
+        <th scope="col">REGISTER DATE</th>
+        <th scope="col">ACTION</th>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+        <th scope="row">000121</th>
+        <td>SALMAN KHAN</td>
+        <td>MALE</td>
+        <td>MUMBAI</td>
+        <td>
+        <!-- Product reviews-->
+        <div class="d-flex justify-content-start small text-warning mb-2">
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+        </div>
+        </td>
+        <td>12/05/2020</td>
+        <td><button type="button" class="btn btn-secondary btn-sm">VIEW</button>
+        </td>
+    </tr>
+    <tr>
+        <th scope="row">000121</th>
+        <td>SALMAN KHAN</td>
+        <td>MALE</td>
+        <td>MUMBAI</td>
+        <td>
+        <!-- Product reviews-->
+        <div class="d-flex justify-content-start small text-warning mb-2">
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+        </div>
+        </td>
+        <td>12/05/2020</td>
+        <td><button type="button" class="btn btn-secondary btn-sm">VIEW</button>
+        </td>
+    </tr><tr>
+        <th scope="row">000121</th>
+        <td>SALMAN KHAN</td>
+        <td>MALE</td>
+        <td>MUMBAI</td>
+        <td>
+        <!-- Product reviews-->
+        <div class="d-flex justify-content-start small text-warning mb-2">
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+        </div>
+        </td>
+        <td>12/05/2020</td>
+        <td><button type="button" class="btn btn-secondary btn-sm">VIEW</button>
+        </td>
+    </tr>
+    <tr>
+        <th scope="row">000121</th>
+        <td>SALMAN KHAN</td>
+        <td>MALE</td>
+        <td>MUMBAI</td>
+        <td>
+        <!-- Product reviews-->
+        <div class="d-flex justify-content-start small text-warning mb-2">
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+          <div class="bi-star-fill"></div>
+        </div>
+        </td>
+        <td>12/05/2020</td>
+        <td><button type="button" class="btn btn-secondary btn-sm">VIEW</button>
+        </td>
+    </tr>
+
+</tbody>
+</table>`;
---
+++ readme.md
@@ -0,0 +1,2 @@
+reports -> sl no, brand, product catagory, product quantity, total price, action
+sellers -> seller id, seller name, gender,city, ratings, register date, action
