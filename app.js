e0f115a3c18d0804416bc60337dbc8c37b4c6870
Debasish Brahma <dbrahma4@outlook.com>
2022-12-23 18:59:19 +0530

    all updated

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict'
+
 //use path module
 const path = require("path");
 //use express module
@@ -7,7 +8,7 @@ const express = require("express");
 const hbs = require("hbs");
 
 //public Routers
-const indexRouter = require("./routes/HomeRouter");
+const indexRouter =  require("./routes/HomeRouter");
 const AboutRouter = require("./routes/AboutRouter");
 const CartRouter = require("./routes/CartRouter");
 const ProductsRouter = require("./routes/ProductsRouter");
@@ -21,7 +22,6 @@ const Ds_ReportRouter = require("./routes/admin/Ds_ReportRouter");
 const Ds_TrackRouter = require("./routes/admin/Ds_TrackRouter");
 const Ds_SellerRouter = require("./routes/admin/Ds_SellerRouter");
 
-
 //app load
 const app = express();
 
@@ -33,7 +33,7 @@ app.set("view engine", "hbs");
 
 
 //admin partials for webpage
-hbs.registerPartials(__dirname + '/views/admin/admin_partials', function (err) { });
+hbs.registerPartials(__dirname + '/views/admin/admin_partials', function (err) {});
 //public partials 
 hbs.registerPartials(path.join(__dirname, '/views/partials'), {
   rename: function (name) {
@@ -42,23 +42,31 @@ hbs.registerPartials(path.join(__dirname, '/views/partials'), {
   }
 });
 
-
 //set public folder as static folder static file
 app.use(express.static("public"));
 
 //Common URL
-app.use(indexRouter, AboutRouter, CartRouter, ProductsRouter, LoginRouter);
+app.use('/', indexRouter);
+app.use('/', AboutRouter);
+app.use('/', CartRouter);
+app.use('/', ProductsRouter);
+app.use('/', LoginRouter);
 
 //Admin URL
-app.use(DashboardRouter, Ds_OrderRouter, Ds_ProductsRouter, Ds_CustomersRouter, Ds_ReportRouter, Ds_TrackRouter, Ds_SellerRouter);
-
+app.use('/', DashboardRouter);
+app.use('/', Ds_OrderRouter);
+app.use('/', Ds_ProductsRouter);
+app.use('/', Ds_CustomersRouter);
+app.use('/', Ds_ReportRouter);
+app.use('/', Ds_TrackRouter);
+app.use('/', Ds_SellerRouter);
 
 
 var server = app.listen(3000, () => {
   //var host = server.address().address
   var port = server.address().port
 
-  // console.log("server is running at http://%s:%s", host, port);
+ // console.log("server is running at http://%s:%s", host, port);
   console.log("server is running at http://localhost:%s", port);
 });
 
