33908617416dcd66adb9bfdb1ec71c1ad662a653
SPYDYHAPPY <spiderhappy5@gmail.com>
2023-01-11 18:19:15 +0530

    update

---
+++ db/userlogin.js
@@ -0,0 +1,84 @@
+const users = [
+    {
+        id: 1,
+        name: 'Debasish Brahma',
+        age: 30,
+        gender: 'male',
+        role: 'admin',
+        email: 'debasish93@gmail.com',
+        password: '12345',
+        username: 'debasish93'
+    },
+    {
+        id: 2,
+        name: 'Prasenjit Behera',
+        age: 24,
+        gender: 'male',
+        role: 'admin',
+        email: 'happy96@gmail.com',
+        password: '12345',
+        username: 'happy96'
+    },
+    {
+        id: 3,
+        name: 'Suresh Patra',
+        age: 35,
+        gender: 'male',
+        role: 'staff',
+        email: 'suresh86@gmail.com',
+        password: '12346',
+        username: 'suresh86'
+    },
+    {
+        id: 4,
+        name: 'Abhinash Mahanta',
+        age: 40,
+        gender: 'male',
+        role: 'staff',
+        email: 'abhinash81@gmail.com',
+        password: '12347',
+        username: 'abhinash81'
+    },
+    {
+        id: 5,
+        name: 'Surema Das',
+        age: 32,
+        gender: 'female',
+        role: 'staff',
+        email: 'surema88@gmail.com',
+        password: '12456',
+        username: 'surema88'
+    },
+    {
+        id: 6,
+        name: 'D Suprbha',
+        age: 33,
+        gender: 'female',
+        role: 'user',
+        email: 'suprabha89@gmail.com',
+        password: '12487',
+        username: 'suprabha89'
+    },
+    {
+        id: 7,
+        name: 'Lokesh Behera',
+        age: 27,
+        gender: 'male',
+        role: 'user',
+        email: 'lokesh884@gmail.com',
+        password: '12247',
+        username: 'lokesh884'
+    },
+    {
+        id: 8,
+        name: 'Sulagna Parkar',
+        age: 29,
+        gender: 'female',
+        role: 'user',
+        email: 'sulagna92@gmail.com',
+        password: '15987',
+        username: 'sulagna92'
+    }
+]
+
+module.exports = { users }
